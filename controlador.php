ad30b37d9ff1096cde4e60f1057871247d233340
Aurelio Guerrero <aurelio.guerrero@gmail.com>
2013-06-28 19:31:33 -0500

    primera prueba con servidor

--- controlador.php
+++ controlador.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_GET['accion']))
 {
-	$con = new mysqli("localhost","aguerrero","12345","pruebas");
+	$con = new mysqli("localhost","qtcfnkg3_adm","zzaaqq11","qtcfnkg3_au");
 	if ($con->connect_errno)
 	{
 		echo "Falla en la conexión a la bd: " . $con->connect_error();
@@ -13,7 +13,7 @@ if(isset($_GET['accion']))
 			case 'consultarTodo':
 			{				
 				$cadena = '{ "datos": [';
-				$resultado = $con->query("SELECT * FROM PERSONA ORDER BY nombre");
+				$resultado = $con->query("SELECT * FROM persona ORDER BY nombre");
 				for($i = 0; $i<= $resultado->num_rows - 1; $i++)
 				{
 					$resultado->data_seek($i);
@@ -29,7 +29,7 @@ if(isset($_GET['accion']))
 			case 'consultar':
 			{
 				$cadena = '{ "respuesta": "exito", "datos": [';
-				$resultado = $con->query('SELECT * FROM PERSONA WHERE id = '.$_GET['id']);			
+				$resultado = $con->query('SELECT * FROM persona WHERE id = '.$_GET['id']);			
 				$resultado->data_seek(0);
 				$fila = $resultado->fetch_assoc();
 				$cadena .= '{"id": '.$fila['id'].', "nombre": "'.$fila['nombre'].'", "apellido": "'.$fila['apellido'].'", "telefono": '.$fila['telefono'].'}';				
@@ -40,12 +40,12 @@ if(isset($_GET['accion']))
 			
 			case 'guardar':
 			{
-				$resultado = $con->query('SELECT * FROM PERSONA WHERE nombre = "'.$_GET['nombre'].'" AND apellido = "'.$_GET['apellido'].'" AND telefono = '.$_GET['telefono'].'');
+				$resultado = $con->query('SELECT * FROM persona WHERE nombre = "'.$_GET['nombre'].'" AND apellido = "'.$_GET['apellido'].'" AND telefono = '.$_GET['telefono'].'');
 				if($resultado->num_rows == 0)
 				{				
-					$resultado = $con->query('INSERT INTO PERSONA(nombre,apellido,telefono) VALUES ("'.$_GET['nombre'].'","'.$_GET['apellido'].'",'.$_GET['telefono'].')');
+					$resultado = $con->query('INSERT INTO persona(nombre,apellido,telefono) VALUES ("'.$_GET['nombre'].'","'.$_GET['apellido'].'",'.$_GET['telefono'].')');
 					$cadena = '{ "respuesta": "exito", "datos": [';
-					$resultado = $con->query('SELECT * FROM PERSONA WHERE nombre = "'.$_GET['nombre'].'" AND apellido = "'.$_GET['apellido'].'" AND telefono = '.$_GET['telefono'].'');			
+					$resultado = $con->query('SELECT * FROM persona WHERE nombre = "'.$_GET['nombre'].'" AND apellido = "'.$_GET['apellido'].'" AND telefono = '.$_GET['telefono'].'');			
 					$resultado->data_seek(0);
 					$fila = $resultado->fetch_assoc();
 					$cadena .= '{"id": '.$fila['id'].', "nombre": "'.$fila['nombre'].'", "apellido": "'.$fila['apellido'].'", "telefono": '.$fila['telefono'].'}';				
@@ -62,16 +62,16 @@ if(isset($_GET['accion']))
 			
 			case "eliminar":
 			{
-				$resultado = $con->query('DELETE FROM PERSONA WHERE id = '.$_GET['id']);
+				$resultado = $con->query('DELETE FROM persona WHERE id = '.$_GET['id']);
 				echo '{ "respuesta": "exito"}';
 				break;	
 			}
 			
 			case "actualizar":
 			{
-				$resultado = $con->query('UPDATE PERSONA SET nombre = "'.$_GET['nombre'].'", apellido = "'.$_GET['apellido'].'", telefono = '.$_GET['telefono'].' WHERE id = '.$_GET['id']);
+				$resultado = $con->query('UPDATE persona SET nombre = "'.$_GET['nombre'].'", apellido = "'.$_GET['apellido'].'", telefono = '.$_GET['telefono'].' WHERE id = '.$_GET['id']);
 				$cadena = '{ "respuesta": "exito", "datos": [';
-				$resultado = $con->query('SELECT * FROM PERSONA WHERE id = '.$_GET['id']);			
+				$resultado = $con->query('SELECT * FROM persona WHERE id = '.$_GET['id']);			
 				$resultado->data_seek(0);
 				$fila = $resultado->fetch_assoc();
 				$cadena .= '{"id": '.$fila['id'].', "nombre": "'.$fila['nombre'].'", "apellido": "'.$fila['apellido'].'", "telefono": '.$fila['telefono'].'}';				
